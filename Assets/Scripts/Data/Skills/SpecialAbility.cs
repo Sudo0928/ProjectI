2156700f9877b5b6cc64fdc602b90a3dbe1ef874
Udi <dnel0605@gmail.com>
2025-02-25 11:11:58 +0900

    [feat] player controller

--- Assets/Scripts/Data/Skills/SpecialAbility.cs
+++ Assets/Scripts/Data/Skills/SpecialAbility.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 
 public abstract class SpecialAbility : MonoBehaviour
 {
-	// TODO 함수 추가
     public abstract void OnAbility(PlayerController pc);
     public abstract void RemoveSkill();
-}
-
-public class TESTSKILL : SpecialAbility
-{
-	public override void OnAbility(PlayerController pc)
-	{
-		throw new System.NotImplementedException();
-	}
-
-	public override void RemoveSkill()
-	{
-		throw new System.NotImplementedException();
-	}
 }
