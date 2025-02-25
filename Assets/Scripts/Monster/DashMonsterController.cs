59c57ce965d56e23d9acb089d03abd8a9de9b300
hgkim0728 <hgkim0728@gmail.com>
2025-02-25 20:02:18 +0900

    돌진 공격 몬스터 이동 수정

--- Assets/Scripts/Monster/DashMonsterController.cs
+++ Assets/Scripts/Monster/DashMonsterController.cs
@@ -7,7 +7,6 @@ public class DashMonsterController : MonsterBasic
 {
     [SerializeField, Tooltip("몬스터의 상대가 변경되는 최소 시간")] protected float minStateChangeTime = 1f;
     [SerializeField, Tooltip("몬스터의 상태가 변경되는 최대 시간")] protected float maxStateChangeTime = 3f;
-    [SerializeField, Tooltip("몬스터 상태 가지 수")] protected int stateCount = 0;
 
     [SerializeField, Tooltip("몬스터 이동속도")] protected float moveSpeed = 2;
 
@@ -18,25 +17,29 @@ public class DashMonsterController : MonsterBasic
     void Start()
     {
         base.Start();
-        stateCount = Enum.GetValues(typeof(MonsterState)).Length;
         stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
     }
 
     void Update()
     {
         // 몬스터의 감지 범위에 플레이어가 있다면 
-        if(monsterState != MonsterState.Trace && monsterState != MonsterState.Attack && stateChangeTime > 0)
+        if (monsterState != MonsterState.Trace && monsterState != MonsterState.Attack)
         {
-            stateChangeTime -= Time.deltaTime;
-        }
+            if(stateChangeTime > 0)
+            {
+                stateChangeTime -= Time.deltaTime;
+            }
         else
-        {
-            stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
+            {
+                stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
+                monsterState = UnityEngine.Random.Range(0, 2) == 0 ? MonsterState.Idle : MonsterState.Move;
+            }
         }
 
         switch(monsterState)
         {
             case MonsterState.Idle:
+                rigid.velocity = Vector2.zero;
                 break;
 
             case MonsterState.Move:
@@ -55,11 +58,20 @@ public class DashMonsterController : MonsterBasic
 
     void MonsterMove()
     {
-        float dirX = UnityEngine.Random.Range(0f, 1f);
-        float dirY = UnityEngine.Random.Range(0f, 1f);
-        moveDir = new Vector2(dirX, dirY).normalized;
-
-        rigid.velocity = moveDir * moveSpeed;
+        if (rigid.velocity != Vector2.zero)
+        {
+            float dirX = UnityEngine.Random.Range(0f, 1f);
+            float dirY = UnityEngine.Random.Range(0f, 1f);
+            moveDir = new Vector2(dirX, dirY).normalized;
+            rigid.velocity = moveDir * moveSpeed;
+        }
+        else
+        {
+            Vector2 v = rigid.velocity;
+            v.x = v.x - Time.deltaTime < 0 ? 0 : v.x - Time.deltaTime;
+            v.y = v.y - Time.deltaTime < 0 ? 0 : v.y - Time.deltaTime;
+            rigid.velocity = v;
+        }
     }
 
     void MonsterAttack()
