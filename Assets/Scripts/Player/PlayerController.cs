f4d19072d51fda680ddd8f41d530bc88ee33c354
Udi <dnel0605@gmail.com>
2025-02-25 14:52:30 +0900

    [feat] player animation
    아이템 습득 애니메이션, 피격, 사망 애니메이션 추가, 전체적인 이동, 공격 애니메이션 수정

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,26 +11,34 @@ public class PlayerController : MonoBehaviour
 	public PlayerData playerData;
 	[SerializeField] GameObject tear;
 
-
 	[SerializeField] SpriteRenderer head;
 	[SerializeField] SpriteRenderer body;
 
-	Animator anim;
+	public Animator anim;
 	Animator bodyAnim;
+	Rigidbody2D rigid;
 
 	bool isAttack = false;
+	bool isMove = false;
+
+
+	[SerializeField, Range(0f, 1f)] float sliderFactor = 0.2f;
+
 
 	private void Awake()
 	{
-		anim = GetComponent<Animator>();
-		bodyAnim = body.gameObject.GetComponent<Animator>();	
 		playerData = new PlayerData(this);
+
+		bodyAnim = body.gameObject.GetComponent<Animator>();
+		rigid = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 	void Start()
     {
         InputSystem.Instance.move.action.performed += OnMove;
 		InputSystem.Instance.space.action.performed += Action;
 	}
+
 	private void Update()
 	{
 		if (moveDir.magnitude > 0) 
@@ -38,46 +46,81 @@ public class PlayerController : MonoBehaviour
 			var pos = transform.position; 
 			float speed = playerData.GetOptionValue(DesignEnums.Option.Speed);
 			transform.position = pos + (new Vector3(moveDir.x, moveDir.y, 0) * speed * Time.deltaTime);
-		} 
+
+			if (isMove == false)
+				moveDir *= Mathf.Pow(sliderFactor, Time.deltaTime);
+		}   
 	}
 
 	void OnMove(InputAction.CallbackContext obj)
     {
+		Vector2 dir = obj.ReadValue<Vector2>();
+		isMove = dir.magnitude > 0;
+		BodyAnim(dir);
 
-		moveDir = obj.ReadValue<Vector2>();
-		body.flipX = moveDir.x < 0;
-
-		if (!isAttack)
-		{
-			head.flipX = moveDir.x < 0;
-			anim.SetFloat("dirX", Math.Abs(moveDir.x));
-			anim.SetFloat("dirY", moveDir.y);
-		}
-		
-		bodyAnim.SetFloat("dirX", Math.Abs(moveDir.x));
-		bodyAnim.SetFloat("dirY", Math.Abs(moveDir.y)); 
-	} 
+	}
 
 	void Action(InputAction.CallbackContext obj)
 	{
 		Vector2 dir = obj.ReadValue<Vector2>();
-		if (dir.magnitude <= 0)
-		{
-			isAttack = false;
-			return;
-		}
-		isAttack = true; 
-
-		head.flipX = dir.x < 0;
-		anim.SetFloat("dirX", Math.Abs(dir.x));
-		anim.SetFloat("dirY", dir.y); 
+		HeadAnim(dir);
 
 		var go = Instantiate<GameObject>(tear);
 		go.transform.position = transform.position;
 		go.GetComponent<TearCtrl>().InitTear(dir, 10.0f, 10.0f);
 	}
 
+	Coroutine onIdleHead = null;
+	void HeadAnim(Vector2 dir)
+	{
+		if (dir.magnitude <= 0)
+		{
+			onIdleHead = StartCoroutine(OnIdleHead());
+			isAttack = false;
+			return;
+		}
 
+		if (onIdleHead != null)
+			StopCoroutine(onIdleHead);
+
+
+		isAttack = true;
+		anim.SetBool("isMove", true);
+
+		head.flipX = dir.x < 0;
+		anim.SetFloat("dirX", Math.Abs(dir.x));
+		anim.SetFloat("dirY", dir.y);
+	}
+	void BodyAnim(Vector2 dir)
+	{
+		bodyAnim.SetBool("isMove", isMove);
+
+		if (dir.magnitude > 0)
+		{
+			moveDir = dir;
+			body.flipX = moveDir.x < 0;
+
+			bodyAnim.SetFloat("dirX", Math.Abs(moveDir.x));
+			bodyAnim.SetFloat("dirY", Math.Abs(moveDir.y));
+		}
+
+
+		if (!isAttack)
+		{
+			head.flipX = moveDir.x < 0;
+			anim.SetFloat("dirX", Math.Abs(moveDir.x));
+			anim.SetBool("isMove", isMove);
+			anim.SetFloat("dirY", moveDir.y);
+		}
+	}
+	
+
+	IEnumerator OnIdleHead()
+	{
+		yield return new WaitForSeconds(0.5f);
+		anim.SetFloat("dirX", -1);
+		anim.SetFloat("dirY", -1);
+	}
 
 }
 
