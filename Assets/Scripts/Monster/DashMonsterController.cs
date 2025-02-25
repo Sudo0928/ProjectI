2f91469e9b6792780368d0eb2bf015b70db5853e
hgkim0728 <hgkim0728@gmail.com>
2025-02-25 19:31:56 +0900

    돌진 공격 몬스터 스크립트 이동 작성

---
+++ Assets/Scripts/Monster/DashMonsterController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashMonsterController : MonsterBasic
+{
+    [SerializeField, Tooltip("몬스터의 상대가 변경되는 최소 시간")] protected float minStateChangeTime = 1f;
+    [SerializeField, Tooltip("몬스터의 상태가 변경되는 최대 시간")] protected float maxStateChangeTime = 3f;
+    [SerializeField, Tooltip("몬스터 상태 가지 수")] protected int stateCount = 0;
+
+    [SerializeField, Tooltip("몬스터 이동속도")] protected float moveSpeed = 2;
+
+    protected float stateChangeTime = 0f;
+
+    private Vector2 moveDir = Vector2.zero;
+
+    void Start()
+    {
+        base.Start();
+        stateCount = Enum.GetValues(typeof(MonsterState)).Length;
+        stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
+    }
+
+    void Update()
+    {
+        // 몬스터의 감지 범위에 플레이어가 있다면 
+        if(monsterState != MonsterState.Trace && monsterState != MonsterState.Attack && stateChangeTime > 0)
+        {
+            stateChangeTime -= Time.deltaTime;
+        }
+        else
+        {
+            stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
+        }
+
+        switch(monsterState)
+        {
+            case MonsterState.Idle:
+                break;
+
+            case MonsterState.Move:
+                MonsterMove();
+                break;
+
+            case MonsterState.Trace:
+                MonsterTrace();
+                break;
+
+            case MonsterState.Attack:
+                MonsterAttack();
+                break;
+        }
+    }
+
+    void MonsterMove()
+    {
+        float dirX = UnityEngine.Random.Range(0f, 1f);
+        float dirY = UnityEngine.Random.Range(0f, 1f);
+        moveDir = new Vector2(dirX, dirY).normalized;
+
+        rigid.velocity = moveDir * moveSpeed;
+    }
+
+    void MonsterAttack()
+    {
+        
+    }
+
+    void MonsterTrace()
+    {
+        
+    }
+}
--- Assets/Scripts/Monster/MonsterBasic.cs
+++ Assets/Scripts/Monster/MonsterBasic.cs
@@ -11,24 +11,6 @@ public enum MonsterState
     Attack
 }
 
-// 이동하는 몬스터만 상속
-public interface IMonsterMove
-{
-    protected void Move();
-}
-
-// 추격하는 몬스터만 상속
-public interface IMonsterTrace
-{
-    protected void Trace();
-}
-
-// 공격하는 몬스터만 상속
-public interface IMonsterAttack
-{
-    protected void Attack();
-}
-
 public class MonsterBasic : MonoBehaviour
 {
     // 몬스터 상태
@@ -37,18 +19,11 @@ public class MonsterBasic : MonoBehaviour
     protected Rigidbody2D rigid;
     protected Animator anim;
 
-    [SerializeField, Tooltip("몬스터의 상대가 변경되는 최소 시간")] protected float minStateChangeTime = 1f;
-    [SerializeField, Tooltip("몬스터의 상태가 변경되는 최대 시간")] protected float maxStateChangeTime = 3f;
-    [SerializeField, Tooltip("몬스터 상태 가지 수")] protected int stateCount = 0;
-
     [Space]
-    [SerializeField, Tooltip("몬스터 이동속도")] protected float moveSpeed = 2;
     [SerializeField, Tooltip("몬스터 최대 체력")] protected int monsterMaxHP = 5;
     // 몬스터 현재 체력
     protected int monsterCurrentHP = 0;
     [SerializeField, Tooltip("몬스터 공격력")] protected int monsterAtk = 1;
-
-    protected float stateChangeTime = 0f;
 
     protected void Awake()
     {
@@ -58,8 +33,6 @@ public class MonsterBasic : MonoBehaviour
 
      protected void Start()
     {
-        stateCount = Enum.GetValues(typeof(MonsterState)).Length;
-        stateChangeTime = UnityEngine.Random.Range(minStateChangeTime, maxStateChangeTime);
         monsterCurrentHP = monsterMaxHP;
     }
 
