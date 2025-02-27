769a76db2fcf72e9533bbe9395fd546f08f299bf
Udi <dnel0605@gmail.com>
2025-02-28 01:50:25 +0900

    [feat] Dry Baby Familiar

---
+++ Assets/Scripts/Data/Skills/DryBaby.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DryBaby : SpecialAbility
+{
+	[SerializeField] GameObject dryBaby;
+	public override void OnAbility(Player player)
+	{
+		var go = Instantiate<GameObject>(dryBaby);
+		go.GetComponent<Familiar_DryBaby>().player = player;
+		go.transform.position = player.transform.position + Vector3.up * 0.5f;
+	} 
+
+	public override void RemoveSkill()
+	{
+		
+	}
+
+
+
+}
